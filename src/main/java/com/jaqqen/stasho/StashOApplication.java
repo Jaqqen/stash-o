87a73415dac3635e13ea7c45b4132880cf30a0b6
Jaqqen <hussain.daniyal@outlook.com>
2021-10-26 11:46:37 +0200

    Initialzation

---
+++ src/main/java/com/jaqqen/stasho/StashOApplication.java
@@ -0,0 +1,13 @@
+package com.jaqqen.stasho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StashOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StashOApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaqqen/stasho/StashOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaqqen.stasho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class StashOApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
